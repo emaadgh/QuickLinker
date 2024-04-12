1c0958f3b72ed0103443fc51acc16e5b3a417cdf
Emaad Ghorbani <emaadghorbani@gmail.com>
2024-04-12 15:45:55 +0330

    - Adds global error handling for project except development environment. - Adds using developer exception page for development environment.

--- QuickLinker/Program.cs
+++ QuickLinker/Program.cs
@@ -24,6 +24,18 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler(appBuilder =>
+    {
+        appBuilder.Run(async context =>
+        {
+            context.Response.StatusCode = 500;
+            await context.Response.WriteAsync("An unexpected fault happened. Try again later.");
+        });
+    });
 }
 
 app.UseHttpsRedirection();
