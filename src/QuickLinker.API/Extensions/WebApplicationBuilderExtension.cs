315813df15baddd89d115ebd66d711da48058685
emaadghorbani <emaadghorbani@gmail.com>
2024-11-13 11:52:08 +0330

    Adds WebApplicationBuilderExtension and cleans up Program.cs.

---
+++ src/QuickLinker.API/Extensions/WebApplicationBuilderExtension.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Metrics;
+using QuickLinker.API.DbContexts;
+using QuickLinker.API.Observability;
+using QuickLinker.API.Services;
+using System.Reflection;
+
+namespace QuickLinker.API.Extensions;
+
+public static class WebApplicationBuilderExtension
+{
+    public static void AddApplicationServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers(options =>
+        {
+            options.ReturnHttpNotAcceptable = true;
+        }).AddNewtonsoftJson();
+
+        builder.Services.Configure<AppSettings>(builder.Configuration);
+        var settings = builder.Configuration.Get<AppSettings>();
+        ArgumentNullException.ThrowIfNull(settings, nameof(settings));
+
+        builder.Services.AddTransient<IQuickLinkerRepository, QuickLinkerRepository>();
+        builder.Services.AddScoped<IShortLinkService, ShortLinkService>();
+        builder.Services.AddSingleton<IQuickLinkerDiagnostic, QuickLinkerDiagnostic>();
+
+        builder.Services.AddDbContext<QuickLinkerDbContext>(options =>
+            options.UseSqlServer(settings.ConnectionStrings.QuickLinkerDbContextConnection));
+
+        builder.Services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = settings.ConnectionStrings.QuickLinkerRedisConnection;
+            options.InstanceName = "QuickLinker";
+        });
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(setupAction =>
+        {
+            var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+
+            setupAction.IncludeXmlComments(xmlCommentsFullPath);
+        });
+    }
+
+    public static void AddOpenTelemetry(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddOpenTelemetry()
+                    .WithMetrics(builder =>
+                    {
+                        builder.AddPrometheusExporter();
+                        builder.AddAspNetCoreInstrumentation();
+                        builder.AddRuntimeInstrumentation();
+                        var meter = new[]
+                        {
+                            "QuickLinkerDiagnostic"
+                        };
+
+                        builder.AddMeter(meter);
+                    });
+    }
+}
--- src/QuickLinker.API/Program.cs
+++ src/QuickLinker.API/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using OpenTelemetry.Metrics;
 using QuickLinker.API;
 using QuickLinker.API.DbContexts;
+using QuickLinker.API.Extensions;
 using QuickLinker.API.Observability;
 using QuickLinker.API.Services;
 using System.Reflection;
@@ -9,52 +10,8 @@ using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers(options =>
-{
-    options.ReturnHttpNotAcceptable = true;
-}).AddNewtonsoftJson();
-
-builder.Services.Configure<AppSettings>(builder.Configuration);
-var settings = builder.Configuration.Get<AppSettings>();
-ArgumentNullException.ThrowIfNull(settings, nameof(settings));
-
-builder.Services.AddDbContext<QuickLinkerDbContext>(options =>
-    options.UseSqlServer(settings.ConnectionStrings.QuickLinkerDbContextConnection));
-
-builder.Services.AddTransient<IQuickLinkerRepository, QuickLinkerRepository>();
-builder.Services.AddScoped<IShortLinkService, ShortLinkService>();
-builder.Services.AddSingleton<IQuickLinkerDiagnostic, QuickLinkerDiagnostic>();
-
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    options.Configuration = settings.ConnectionStrings.QuickLinkerRedisConnection;
-    options.InstanceName = "QuickLinker";
-});
-
-builder.Services.AddOpenTelemetry()
-                    .WithMetrics(builder =>
-                    {
-                        builder.AddPrometheusExporter();
-                        builder.AddAspNetCoreInstrumentation();
-                        builder.AddRuntimeInstrumentation();
-                        var meter = new[]
-                        {
-                            "QuickLinkerDiagnostic"
-                        };
-
-                        builder.AddMeter(meter);
-                    });
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(setupAction =>
-{
-    var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
-
-    setupAction.IncludeXmlComments(xmlCommentsFullPath);
-});
+builder.AddApplicationServices();
+builder.AddOpenTelemetry();
 
 var app = builder.Build();
 
